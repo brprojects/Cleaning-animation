4d446e14f6537c090f2006ac722b3e2ccd220872
brprojects <benjamin.rienecker@gmail.com>
2022-03-23 20:28:37 +0000

    scanner drone verification

--- maths.py
+++ maths.py
@@ -1,15 +1,11 @@
-import numpy as np
+dirty_windows = [8,3,5]
+dirty_windows_remove = list(map(lambda n: n +1 , dirty_windows))
+print(dirty_windows_remove)
+x = range(15)
+y = [i for i in x]
+for i in dirty_windows:
+    y.remove(i+1)
+for i in y:
+    if i in dirty_windows:
+        print(i)
 
-x = np.random.random_sample(5)
-y = ['']*5
-print(y)
-print(x)
-
-for i in range(len(x)):
-    if x[i] <= 0.6 :
-        y[i] = "circle"
-    elif x[i] <= 0.8 and x[i] > 0.6 :
-        y[i] = "square"
-    else:
-        y[i] = "triangle"
-print(y)
